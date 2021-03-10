266270e196d8c1104bf2508c0e1e8c8cf34117b3
faultable <hi@faultable.dev>
2021-03-10 13:21:17 +0700

    feat: create evlfctry.pro

---
+++ evlfctry.pro.lua
@@ -0,0 +1,13 @@
+-- zone = _a (evlfctry.pro)
+
+-- NS records
+ns(_a, "ns01.edgyscale.host")
+ns(_a, "ns02.edgyscale.host")
+ns(_a, "ns03.edgyscale.host")
+ns(_a, "ns04.edgyscale.host")
+
+--- A records
+a(_a, "192.46.225.58")
+
+--- TXT records
+txt("_github-challenge-evilfactorylabs", "877b53fd61")
