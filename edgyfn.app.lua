84f42f027dcdd4314aa3c7d488dd8b10919f870c
faultable <hi@faultable.dev>
2021-03-14 13:14:31 +0700

    feat: create edgyfn.app

---
+++ edgyfn.app.lua
@@ -0,0 +1,10 @@
+-- zone = _a (edgyfn.app)
+
+-- NS records
+ns(_a, "ns01.edgyscale.host")
+ns(_a, "ns02.edgyscale.host")
+ns(_a, "ns03.edgyscale.host")
+ns(_a, "ns04.edgyscale.host")
+
+-- A records
+a(_a, "95.111.193.77")
