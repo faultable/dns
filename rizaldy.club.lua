4e79969723c1397cd3a1b5828c171fcde127ee59
fariz <hi@faultable.dev>
2021-04-18 10:27:02 +0000

    feat: update

--- rizaldy.club.lua
+++ rizaldy.club.lua
@@ -14,6 +14,7 @@ a("sso", "143.198.198.198")
 cname("key1._domainkey", "key1.rizaldy.club._domainkey.migadu.com")
 cname("key2._domainkey", "key2.rizaldy.club._domainkey.migadu.com")
 cname("key3._domainkey", "key3.rizaldy.club._domainkey.migadu.com")
+cname("fariz", "8f2d4ace.netlify.app")
 cname("karina", "5472ff6bbe82ae5bb2c53c910a4d30c2.netlify.app")
 
 -- MX records
