eb9b957d3a80cb1d2ff3916351a078a34b2b0f97
fariz <hi@faultable.dev>
2021-05-05 12:12:23 +0000

    feat: update evlfctry.pro

--- evlfctry.pro.lua
+++ evlfctry.pro.lua
@@ -6,8 +6,8 @@ ns(_a, "ns02.edgyscale.host")
 ns(_a, "ns03.edgyscale.host")
 ns(_a, "ns04.edgyscale.host")
 
---- A records
-a(_a, "192.46.225.58")
+--- ALIAS records
+alias(_a, "evilfactorylabs.github.io")
 
 --- TXT records
 txt("_github-challenge-evilfactorylabs", "877b53fd61")
