1dbd5736d8ecd6ea63399592b469763d314e8d30
fariz <hi@faultable.dev>
2021-05-25 07:12:35 +0000

    feat: update

--- edgy.network.lua
+++ edgy.network.lua
@@ -38,7 +38,7 @@ mx("relay", "mx1.simplelogin.co", 10)
 mx("relay", "mx2.simplelogin.co", 20)
 
 -- TXT records
-txt("_dmarc.relay", "v=DMARC1; p=quarantine; pct=100; adkim=r; aspf=s")
+txt("_dmarc.relay", "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s")
 txt("relay", "v=spf1 include:simplelogin.co -all")
 
 txt("_github-challenge-80659ef293e67c01e5db9a47632c42cb",	"c7ac584eac")
