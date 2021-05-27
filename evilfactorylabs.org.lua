228f198e24bf12d33a8ad2285e1cfc15fdffc0df
fariz <hi@faultable.dev>
2021-05-27 11:59:06 +0000

    feat: update

--- evilfactorylabs.org.lua
+++ evilfactorylabs.org.lua
@@ -6,8 +6,9 @@ ns(_a, "ns02.edgyscale.host")
 ns(_a, "ns03.edgyscale.host")
 ns(_a, "ns04.edgyscale.host")
 
--- ALIAS records
-alias(_a, "evilfactorylabs-org.netlify.app")
+-- A records
+a(_a, "128.199.89.41")
 
--- CNAME records
-cname("www", "evilfactorylabs-org.netlify.app")
+-- AAAA records
+aaaa(_a, "2400:6180:0:d0::d7:4001")
+
