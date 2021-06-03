a88a504b5939d8a318ddb6724eea2d336810424d
faultable <oss@faultable.dev>
2021-06-03 22:18:59 +0700

    feat: add mailgun things

--- evilfactorylabs.org.lua
+++ evilfactorylabs.org.lua
@@ -12,3 +12,13 @@ a(_a, "128.199.89.41")
 -- AAAA records
 aaaa(_a, "2400:6180:0:d0::d7:4001")
 
+--- TXT records
+txt("mg", "v=spf1 include:mailgun.org ~all")
+txt("mta_domainkey.mg", "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDE2EiU3PrqyJsZfz1MM+LRKTqy9tkjCeVeqY8pZjbOskZ/QdBQI308l1i7i2AQ16GvJK16NgnyT/g+kcCwFHLt5rmh4h/1jyO/Jl6Q+s9Sfqgo3RMZc9jQ5I+6hwU47wU1zLrvrPefuGCwF+sCvFvRBS80fQP15GTAoSuiZN4LyQIDAQAB")
+
+--- MX records
+mx("mg", "mxa.eu.mailgun.org", 10)
+mx("mg", "mxb.eu.mailgun.org", 10)
+
+--- CNAME records
+cname("email.mg", "eu.mailgun.org")
