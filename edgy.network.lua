de9ec6f0b12c1c799810216a7ddc3b6433a54238
faultable <oss@faultable.dev>
2021-06-20 07:27:02 +0700

    feat: update

--- edgy.network.lua
+++ edgy.network.lua
@@ -7,7 +7,7 @@ ns(_a, "ns03.edgyscale.host")
 ns(_a, "ns04.edgyscale.host")
 
 -- A records
-a(_a, "128.199.89.41")
+a(_a, "76.76.21.21")
 a("dns", "27.112.79.80")
 a("*.dns", "27.112.79.80")
 a("u", "128.199.89.41")
@@ -34,6 +34,7 @@ aaaa("ipfs", "2400:6180:0:d0::d7:4001")
 aaaa("api.ipfs", "2400:6180:0:d0::d7:4001")
 
 -- CNAME records
+cname("www", "cname.vercel-dns.com")
 cname("dkim._domainkey.relay", "dkim._domainkey.simplelogin.co")
 
 -- MX records
