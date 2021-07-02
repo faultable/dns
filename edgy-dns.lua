7cca93efca13aa588544053721faec7cfa22f6fd
faultable <oss@faultable.dev>
2021-07-02 10:36:27 +0700

    feat: update

---
+++ edgy-dns.lua
@@ -0,0 +1,15 @@
+-- zone = _a (edgy-dns.com)
+
+-- NS records
+ns(_a, "ns01.edgyscale.host")
+ns(_a, "ns02.edgyscale.host")
+ns(_a, "ns03.edgyscale.host")
+ns(_a, "ns04.edgyscale.host")
+
+-- A records
+a(_a, "27.112.79.80")
+a(_a, "27.112.79.208")
+
+-- AAAA records
+aaaa(_a, "2001:470:ecbf::666")
+aaaa(_a, "2001:470:ecbf::53")
